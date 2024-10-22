dea1ebd13ca375bdfa6b8949129f522e5d08a7fb
Simran Saxena <simran13104@iiitd.ac.in>
2024-10-22 11:28:27 -0700

    Time: 4 ms (96.87%) | Memory: 16.6 MB (18.84%) - LeetSync

---
+++ 1585-the-kth-factor-of-n/the-kth-factor-of-n.py
@@ -0,0 +1,24 @@
+class Solution:
+    def kthFactor(self, n: int, k: int) -> int:
+
+        # Find factors of n
+        # Sort factors of n
+        # Return kth factor or -1 
+        factors = []
+        if n < 0: # check if n is positive
+            return -1 
+            
+        if k < 0: # check if k is positive
+            return -1  
+            
+        for i in range(1, n+1):
+            if n % i == 0:
+                factors.append(i)
+        
+        print(factors)
+        factors.sort()
+        
+        if len(factors) > k-1:
+            return factors[k-1]
+        else:
+            return-1

3a81202c3f558044fae86e28c750db3005734459
Simran Saxena <simran13104@iiitd.ac.in>
2024-10-24 12:43:40 -0700

    Time: 5 ms (95.68%) | Memory: 16.7 MB (7.27%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.py
+++ 12-integer-to-roman/integer-to-roman.py
@@ -20,4 +20,17 @@ class Solution:
         # Join the result list into a single string and return it
         return ''.join(result)
 
+        # eg 3749
+        # 3749 > 1000
+        # res = M
+        # num = 2749
+        # 2749 > 1000 -> res = MM, num = 1749
+        # 1749 > 1000 -> res = MMM, num = 749
+        # 749 > 500 -> res = MMMD , num = 249
+        # 249 > 100 -> res MMMDC, num = 149
+        # 149 > 100 -> res MMMDCC, num = 49
+        # 49 > 40 -> res MMMDCCXL, num = 9
+        # 9 == 9, res = MMMDCCXLIX
+
+
         

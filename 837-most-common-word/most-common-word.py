182c3a12b3ed9260f4c7f99b33af4b2270fc47bf
Simran Saxena <simran13104@iiitd.ac.in>
2024-10-25 12:28:42 -0700

    Time: 3 ms (96.83%) | Memory: 16.8 MB (10.66%) - LeetSync

---
+++ 837-most-common-word/most-common-word.py
@@ -0,0 +1,20 @@
+import re
+from collections import Counter
+
+
+class Solution:
+    def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
+
+        # convert paragraph to lower
+        # get rid of punctuations
+        paragraph = re.sub(r'[^\w\s]', ' ', paragraph.lower())  # Replace non-word characters with spaces
+
+        paragraph = paragraph.split()
+
+        filtered_words = [word for word in paragraph if word not in banned]
+
+        word_counts = Counter(filtered_words)
+        most_common_word, _ = word_counts.most_common(1)[0]
+
+        return most_common_word
+                

240c26e1219b327a9814e311cd33c192e3755195
Simran Saxena <simran13104@iiitd.ac.in>
2024-10-27 07:21:33 -0700

    Time: 7371 ms, Memory: 186.5 MB - LeetHub

---
+++ word-ladder/word-ladder.py
@@ -0,0 +1,41 @@
+# 127. Word Ladder
+
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+
+        if endWord not in wordList:
+            return 0
+
+        nei = collections.defaultdict(list)
+        wordList.append(beginWord)
+
+        # build adjacency list
+
+        for word in wordList:
+            for j in range(len(wordList)):
+                pattern = word[:j] + '*' + word[j+1:]
+                nei[pattern].append(word)
+
+        visit = set([beginWord])
+        q = collections.deque([beginWord])
+        res = 1
+
+        while q:
+            for i in range(len(q)):
+                word = q.popleft()
+                if word == endWord:
+                    return res
+                for j in range(len(word)):
+                    pattern = word[:j] + '*' + word[j+1:]
+                    for neiWord in nei[pattern]:
+                        if neiWord not in visit:
+                            visit.add(neiWord)
+                            q.append(neiWord)
+
+                    # get all neighbors of this word
+            res += 1
+        return 0
+
+
+
+        

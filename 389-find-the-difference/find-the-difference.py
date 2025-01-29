3dd2e667855d77be3034f7d5eefccb9b10f52923
Simran Saxena <simran13104@iiitd.ac.in>
2025-01-29 21:26:01 +0530

    Time: 3 ms (53.45%) | Memory: 17.5 MB (80.38%) - LeetSync

--- 389-find-the-difference/find-the-difference.py
+++ 389-find-the-difference/find-the-difference.py
@@ -1,11 +1,23 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        count_s, count_t = Counter(s), Counter(t)
+        # count_s, count_t = Counter(s), Counter(t)
 
-        for c in count_t:
-            if c not in count_s:
-                return c
-            if count_s[c] < count_t[c]:
-                return c
+        # for c in count_t:
+        #     if c not in count_s:
+        #         return c
+        #     if count_s[c] < count_t[c]:
+        #         return c
+
+        # approach 2
+        sum_s, sum_t = 0, 0
+
+        for c in s:
+            sum_s += ord(c)
+
+        for c in t:
+            sum_t += ord(c)
+
+        return chr(sum_t - sum_s)
+
 
         

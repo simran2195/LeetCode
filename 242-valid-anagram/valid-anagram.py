d0ce2f6ed40dbd0f1ec3890c6bee14d1dfd6842f
Simran Saxena <simran13104@iiitd.ac.in>
2025-02-07 19:16:54 +0530

    Time: 7 ms (90.17%) | Memory: 17.8 MB (59.09%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,9 +1,25 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         
+        # Way 1
+        # Just use the Counter function
+        return Counter(s) == Counter(t)
+        
+        # Way 2
         # sort both strings andcompare
-        # O(nlog(n))
-        if sorted(s) == sorted(t):
-            return True
-        else:
-            return False
+        # O(nlogn + mlogm)
+        # if sorted(s) == sorted(t):
+        #     return True
+        # else:
+        #     return False
+
+        # Way 2
+        # 
+        # if len(s) != len(t):
+        #     return False
+
+        # count_s, count_t = {}, {}
+
+
+
+        

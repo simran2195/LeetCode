7eb4445384a7b73c26dde64883c21e96b56893b5
Simran Saxena <simran13104@iiitd.ac.in>
2025-02-07 19:57:53 +0530

    Time: 15 ms (43.52%) | Memory: 17.7 MB (89.07%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -3,23 +3,35 @@ class Solution:
         
         # Way 1
         # Just use the Counter function
-        return Counter(s) == Counter(t)
+        # O(n+m) time complexity
+        # O(26)*2 space complexity
+        # return Counter(s) == Counter(t)
         
         # Way 2
         # sort both strings andcompare
         # O(nlogn + mlogm)
+
         # if sorted(s) == sorted(t):
         #     return True
         # else:
         #     return False
 
-        # Way 2
+        # Way 3
         # 
-        # if len(s) != len(t):
-        #     return False
+        if len(s) != len(t):
+            return False
 
-        # count_s, count_t = {}, {}
+        count_s, count_t = {}, {}
+        for i in range(len(s)):
+            count_s[s[i]] = 1 + count_s.get(s[i], 0)
+            count_t[t[i]] = 1 + count_t.get(t[i], 0)
 
+
+        for ele in count_s:
+            if count_s[ele] != count_t.get(ele, 0):
+                return False
+        
+        return True
 
 
         

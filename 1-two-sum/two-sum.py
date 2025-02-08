1cd3dfe19c4897d32d1deffc6133fda32566951c
Simran Saxena <simran13104@iiitd.ac.in>
2025-02-08 12:26:16 +0530

    Time: 0 ms (100.00%) | Memory: 18.9 MB (45.29%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -9,26 +9,27 @@ class Solution:
         # Method 1
         # O(n^2) time complexity
         # Brute Force method
-        for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
-                if nums[i]+nums[j] == target:
-                    return [i, j]
+
+        # for i in range(len(nums)):
+        #     for j in range(i+1, len(nums)):
+        #         if nums[i]+nums[j] == target:
+        #             return [i, j]
+
+        # Method 2
+        # 1 pass using a hashmap
+        # Time complexity:
+        # Space complexity:
+
+        prev_values = {}
+        for i, val in enumerate(nums):
+            diff = target - val
+            if diff in prev_values:
+                return [prev_values[diff], i]
+
+            prev_values[val] = i
+
         
         
-        # prevMap = {}
-    
-        # for i, n in enumerate(nums) :
-        #     diff = target-n
-        #     # print("i = ", i, "n = ", n)
-        #     # print("diff = ", diff)
-        #     # print(prevMap) 
-        #     if diff in prevMap:
-        #         # print(prevMap[diff], i)
-        #         return [prevMap[diff], i]
-
-        #     prevMap[n] = i
-        #     # print(f"Added prevMap[{n}] = {i}", "\n" )
-
-        # return
+     
 
 

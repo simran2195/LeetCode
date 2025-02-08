8573ac5ebecc87da46a006488c1158711025a5c6
Simran Saxena <simran13104@iiitd.ac.in>
2025-02-08 13:40:49 +0530

    Time: 27 ms (5.08%) | Memory: 18.3 MB (40.34%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,37 +1,42 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
 
-        # to lower case
-        s = s.lower()
+        # # to lower case
+        # s = s.lower()
 
-        s_new = []
+        # s_new = []
+        # #ignore all non-alphanumeric characters
+        # for c in s:
+        #     if c.isalnum():
+        #         s_new.append(c)
+
+        # s_new = "".join(s_new)
+        # return s_new[::-1] == s_new
+
+
+        s_new = ""
+
         #ignore all non-alphanumeric characters
         for c in s:
-            if c.isalnum():
-                s_new.append(c)
+            if self.isalnumcheck(c):
+                s_new += c.lower()
 
-        s_new = "".join(s_new)
-        return s_new[::-1] == s_new
+        # using two pointers
+        l, r = 0, len(s_new)-1
 
-        # s = s.lower()
-        # new_s = ""
-
-        # # remove all non-alnum chars
-        # for char in s:
-        #     if char.isalnum():
-        #         new_s += char
-
-        # # check for palindrome
-
-        # l, r = 0, len(new_s)-1
-
-        # while l < r:
-        #     if new_s[l] == new_s[r]:
-        #         l, r = l+1, r-1
-
-        #     else:
-        #         return False
+        while l < r:
+            if s_new[l] == s_new[r]:
+                l, r = l+1, r-1
+            else:
+                return False
         
-        # return True
+        return True
+
+    def isalnumcheck(self, c):
+        if (ord(c) >= ord('0') and ord(c) <= ord('9')) or (ord(c) >= ord('a') and ord(c) <= ord('z')) or (ord(c) >= ord('A') and ord(c) <= ord('Z')):
+            return True
+        else:
+            return False
+
     
         

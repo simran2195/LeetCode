b644119dd6a2b6b98451c8aba8ab85621ceb07e5
Simran Saxena <simran13104@iiitd.ac.in>
2025-02-08 23:57:14 +0530

    Time: 11 ms (4.15%) | Memory: 17.9 MB (45.04%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -4,23 +4,33 @@
 class Solution:
     def isValid(self, s: str) -> bool:
 
+        # Method 1: Brute force
+        # O(n^2)
+        while "()" in s or "[]" in s or "{}" in s:
+            s = s.replace("()", "")
+            s = s.replace("[]", "")
+            s = s.replace("{}", "")
+
+        return s == ""
+
+
         valid = {')' : '(',  '}' : '{', ']' : '['}
         stack = []
 
-        for char in s:
-            if char in valid: # if there's a closing bracket, check for opening bracket
-                if stack:
-                    top_element = stack.pop() 
-                else:
-                    top_element = '#' # assign a dummy value
+        # for char in s:
+        #     if char in valid: # if there's a closing bracket, check for opening bracket
+        #         if stack:
+        #             top_element = stack.pop() 
+        #         else:
+        #             top_element = '#' # assign a dummy value
 
-                if valid[char] != top_element:
-                    return False
+        #         if valid[char] != top_element:
+        #             return False
                 
-            else:
-                stack.append(char)
+        #     else:
+        #         stack.append(char)
 
-        return not stack
+        # return not stack
 
 
 

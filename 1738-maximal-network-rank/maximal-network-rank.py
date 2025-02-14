e95870e4a713f8ce81bcf41f5febafd22b9ee924
Simran Saxena <simran13104@iiitd.ac.in>
2025-02-15 00:48:10 +0530

    Time: 55 ms (29.83%) | Memory: 20.1 MB (5.40%) - LeetSync

--- 1738-maximal-network-rank/maximal-network-rank.py
+++ 1738-maximal-network-rank/maximal-network-rank.py
@@ -2,17 +2,20 @@ class Solution:
     def maximalNetworkRank(self, n: int, roads: List[List[int]]) -> int:
 
         degree = defaultdict(int)
+        road_set = set() # for efficiency 
 
         for road in roads:
             a, b = road
             degree[a] += 1
             degree[b] += 1
+            road_set.add((a,b))
+            road_set.add((b,a))
 
         max_rank = 0
         for i in range(0, n):
             for j in range(i+1, n):
                 rank = degree[i] + degree[j]
-                if [i,j] in roads or [j,i] in roads and i!=j:
+                if (i, j) in road_set or (j, i) in road_set:
                     rank -= 1
                 
                 max_rank = max(rank, max_rank)

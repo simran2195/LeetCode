7b4775c1b0ed2d3b7012d4af13ef10dd0f256664
Simran Saxena <simran13104@iiitd.ac.in>
2025-02-17 01:26:48 +0530

    Time: 4 ms (71.04%) | Memory: 21.3 MB (62.15%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -5,18 +5,33 @@ class Solution:
         # 2) Get elements with top k frequencies
         
         # method 1 - hashmap of frequencies
-        freq_map = defaultdict(int)
+        # freq_map = defaultdict(int)
 
-        for ele in nums:
-            freq_map[ele] += 1
+        # for ele in nums:
+        #     freq_map[ele] += 1
 
-        res = sorted(freq_map, key=freq_map.get, reverse=True)[:k]
-        return res
+        # res = sorted(freq_map, key=freq_map.get, reverse=True)[:k]
+        # return res
 
         
 
         # method 2 - collections.counter
-
         # heap of frequencies
+
+        freq_map = Counter(nums)
+        heap = []
+        for num, freq in freq_map.items():
+            heapq.heappush(heap, (-1*freq, num))
+            # if len > k, pop last element
+            
+
+        res = []
+        for i in range(0,k):
+            res.append(heapq.heappop(heap)[1])
+
+        return res
+
+
+
         
 

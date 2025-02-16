272cb45dcc3d65b78b31d0726a84750a22836c53
Simran Saxena <simran13104@iiitd.ac.in>
2025-02-17 02:04:19 +0530

    Time: 13 ms (14.67%) | Memory: 24.1 MB (20.13%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -18,18 +18,44 @@ class Solution:
         # method 2 - collections.counter
         # heap of frequencies
 
-        freq_map = Counter(nums)
-        heap = []
-        for num, freq in freq_map.items():
-            heapq.heappush(heap, (-1*freq, num))
-            # if len > k, pop last element
+        # freq_map = Counter(nums)
+        # heap = []
+        # for num, freq in freq_map.items():
+        #     heapq.heappush(heap, (-1*freq, num))
+        #     # if len > k, pop last element
             
 
+        # res = []
+        # for i in range(0,k):
+        #     res.append(heapq.heappop(heap)[1])
+
+        # return res
+
+        # method 3
+        # Bucket sort
+        freq_map = defaultdict(int)
+        for ele in nums:
+            freq_map[ele] += 1        
+        
+        n = len(nums)
+        freq_count = [[] for i in range(0,n+1)]
+
+        for num, freq in freq_map.items():
+            freq_count[freq].append(num)
+
         res = []
-        for i in range(0,k):
-            res.append(heapq.heappop(heap)[1])
+        for i in range(n, 0, -1):
+            for num in freq_count[i]:
+                res.append(num)
+                if len(res) == k:
+                    return res
 
         return res
+
+
+
+
+
 
 
 

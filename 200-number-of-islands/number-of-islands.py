ae77ab156252196646170501acc201f90187a0fb
Simran Saxena <simran13104@iiitd.ac.in>
2025-02-20 13:20:00 +0530

    Time: 317 ms (12.10%) | Memory: 25.3 MB (31.45%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,37 +1,39 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        # base case of empty grid
+        
         if not grid:
             return 0
 
         rows, cols = len(grid), len(grid[0])
-        visited = set() # can use 2D grid also, but sets are easier
-        islands = 0
+        visited = set()
+        num_islands = 0
 
-        def bfs(r, c): #bfs is iterative and not recursive 
+        def bfs(r, c):
+            # look in horizontal and vertical 1 step of r, c. if not visited, call bfs
+
             q = collections.deque()
-            visited.add((r,c)) # add this position to the visited set
-            q.append((r, c))
+            q.append((r,c))
+            visited.add((r,c))
+            directions = [[0,1], [0,-1], [1,0], [-1,0]]
 
             while q:
-                row, col = q.popleft() # pop this position and check its adjacent positions
-                directions = [[1, 0 ], [-1,0], [0, 1], [0, -1]]
-
+                row, col = q.popleft()
                 for dr, dc in directions:
                     r, c = row+dr, col+dc
-                    if (r in range(rows) and c in range(cols) and grid[r][c] == "1" and (r, c) not in visited):
-                        q.append((r, c))
-                        visited.add((r, c))
+                    # check if is it 1, not in visited and index check
+                    if (r in range(rows) and c in range(cols) and (r, c) not in visited and grid[r][c] == "1"):
+                        q.append((r,c))
+                        visited.add((r,c))
 
-
+                
 
         for r in range(rows):
             for c in range(cols):
-                if grid[r][c] == "1" and (r,c ) not in visited: # the grid is made of strings
-                    bfs(r,c) #run bfs on this cell
-                    islands += 1 # only add if there's a 1 and we haven't visited the island
+                if grid[r][c] == "1" and (r,c) not in visited:
+                    bfs(r,c)
+                    num_islands += 1
 
-        return islands
+        return num_islands
 
-
+            
 
